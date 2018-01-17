dc3ef3aac2a89f11b78122759a25bec337bd9cef
tanajipochapure <pochapure@gmail.com>
2018-01-17 17:31:33 +0530

    changes in mainactivity

--- app/src/main/java/com/example/appeal_android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/appeal_android/myapplication/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test comment..!   tanaji pochapure adsdsfd 
+        //test comment..!   tanaji pochapure adsdsfd
     }
 }
